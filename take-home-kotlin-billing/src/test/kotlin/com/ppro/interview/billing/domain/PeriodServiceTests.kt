d86c41c97fb656ca897c2968910a8d8c513e7179
Daniel Gimenes <danielcgimenes@gmail.com>
2023-05-23 18:13:55 -0300

    Tests for PeriodService that handles string date input and replies with periodId

---
+++ take-home-kotlin-billing/src/test/kotlin/com/ppro/interview/billing/domain/PeriodServiceTests.kt
@@ -0,0 +1,62 @@
+package com.ppro.interview.billing.domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PeriodServiceTests {
+
+    @Test
+    fun periodIdOfDateWhenDateSample1() {
+        val inputDate = "24th Jan 2019"
+
+        val expected = "periodId = 2019-4"
+
+        assertEquals(expected, PeriodService().periodIdOfDate(inputDate))
+    }
+
+    @Test
+    fun periodIdOfDateWhenDateSample2() {
+        val inputDate = "2nd Feb 2019"
+
+        val expected = "periodId = 2019-7"
+
+        assertEquals(expected, PeriodService().periodIdOfDate(inputDate))
+    }
+
+    @Test
+    fun periodIdOfDateWhenFirstDay() {
+        val inputDate = "1st Mar 2023"
+
+        val expected = "periodId = 2023-11"
+
+        assertEquals(expected, PeriodService().periodIdOfDate(inputDate))
+    }
+
+    @Test
+    fun periodIdOfDateWhenSecondDay() {
+        val inputDate = "2nd Mar 2023"
+
+        val expected = "periodId = 2023-11"
+
+        assertEquals(expected, PeriodService().periodIdOfDate(inputDate))
+    }
+
+    @Test
+    fun periodIdOfDateWhenThirdDay() {
+        val inputDate = "3rd Mar 2023"
+
+        val expected = "periodId = 2023-11"
+
+        assertEquals(expected, PeriodService().periodIdOfDate(inputDate))
+    }
+
+    @Test
+    fun periodIdOfDateWhenOtherDay() {
+        val inputDate = "24th Mar 2023"
+
+        val expected = "periodId = 2023-14"
+
+        assertEquals(expected, PeriodService().periodIdOfDate(inputDate))
+    }
+
+}
